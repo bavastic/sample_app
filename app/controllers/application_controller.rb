8e6b8d139469d8d4759d133d69049dc0532b3f5f
Mikhail Mandronov <mikhail.mandronov@riskmethods.net>
2019-05-17 10:40:36 +0200

    Add a method to convert camel case keys of hash to snake case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  def hash_keys_to_snake_case(hash:)
+    hash.each_with_object({}) do |(k, v), mem|
+      mem[k.underscore.to_sym] = v
+    end
+  end
 end
