f308a74bce2ea42d691f2a22288ee13e2c3442f4
Mikhail Mandronov <mikhail.mandronov@riskmethods.net>
2019-05-17 17:23:09 +0200

    Annotate

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -4,7 +4,7 @@
 #
 #  id             :bigint           not null, primary key
 #  parent_id      :bigint
-#  name           :string
+#  name           :string           not null
 #  products_count :integer
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
