001f55682c9e8666dfc579cd576259a27f8a3c8f
Mikhail Mandronov <mikhail.mandronov@riskmethods.net>
2019-05-17 17:17:51 +0200

    Add test coverage for the models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,7 +6,7 @@
 #
 #  id             :bigint           not null, primary key
 #  parent_id      :bigint
-#  name           :string
+#  name           :string           not null
 #  products_count :integer
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
@@ -16,7 +16,9 @@
 require 'rails_helper'
 
 RSpec.describe Category do
-  describe '.save' do
+  subject { described_class }
+
+  describe '#save' do
     context 'with valid params' do
       let(:model) { build(:category) }
 
@@ -27,17 +29,37 @@ RSpec.describe Category do
       let!(:model) { described_class.new }
       before { model.validate }
 
-      it { expect(model.errors.messages.size).to be 4 }
-      it { expect(model.errors.messages[:key]).to eq(['can\'t be blank']) }
+      it { expect(model.errors.messages.size).to be 1 }
       it { expect(model.errors.messages[:name]).to eq(['can\'t be blank']) }
-      it { expect(model.errors.messages[:internal]).to eq(['is not included in the list']) }
-      it { expect(model.errors.messages[:auth_mode]).to eq(['must exist']) }
+    end
+
+    context 'when the category parent id refer to own id' do
+      let!(:model) { create(:category) }
+
+      it 'raise the validation error' do
+
+        expect { model.update!(parent: model) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
     end
   end
 
-  describe '.destroy' do
-    let(:model) { create(:category) }
+  describe '#destroy' do
+    let!(:model) { create(:category) }
+    let!(:child) { create(:category, parent: model) }
+    let!(:product) { create(:product, category: child) }
 
-    it { expect { model.destroy }.to change { Category.count }.by(-1) }
+    it 'destroys with child categories relation' do
+      expect { model.destroy }.to change { Category.count }.by(-2)
+    end
+
+    it 'destroys with products' do
+      expect { model.destroy }.to change { Product.count }.by(-1)
+    end
+  end
+
+  describe '.search_by_query' do
+    let(:search) { 'search' }
+    let!(:model) { create(:category, name: "#{Faker::Name.name}#{search}#{Faker::Name.name}") }
+    it { expect(subject.search_by(query: search.upcase)).to include(model) }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@
 #
 #  id               :bigint           not null, primary key
 #  category_id      :bigint
-#  name             :string
+#  name             :string           not null
 #  price            :decimal(, )
 #  currency         :string           default("EUR")
 #  display_currency :string           default("EUR")
@@ -18,28 +18,47 @@
 require 'rails_helper'
 
 RSpec.describe Product do
-  describe '.save' do
+  subject { described_class }
+
+  describe '#save' do
     context 'with valid params' do
-      let(:model) { build(:product) }
+      let(:category) { create(:category) }
+      let(:model) { build(:product, category: category) }
 
       it { expect { model.save }.to change { Product.count }.by(1) }
+
+      it 'increments the products_count on category' do
+        expect { model.save }.to change { category.reload.products_count }.by(1)
+      end
     end
 
     context 'without required params' do
       let!(:model) { described_class.new }
       before { model.validate }
 
-      it { expect(model.errors.messages.size).to be 4 }
-      it { expect(model.errors.messages[:key]).to eq(['can\'t be blank']) }
+      it { expect(model.errors.messages.size).to be 2 }
+
       it { expect(model.errors.messages[:name]).to eq(['can\'t be blank']) }
-      it { expect(model.errors.messages[:internal]).to eq(['is not included in the list']) }
-      it { expect(model.errors.messages[:auth_mode]).to eq(['must exist']) }
+      it { expect(model.errors.messages[:category]).to eq(['must exist']) }
+    end
+
+    context 'without price' do
+      let!(:model) { create(:product, price: nil) }
+
+      it 'fills out the default value' do
+        expect(model).to have_attributes({ price: 0.0 })
+      end
     end
   end
 
-  describe '.destroy' do
-    let(:model) { create(:product) }
-
+  describe '#destroy' do
+    let!(:model) { create(:product) }
     it { expect { model.destroy }.to change { Product.count }.by(-1) }
+  end
+
+  describe '.search_by_query' do
+    let(:search) { 'search' }
+    let!(:model) { create(:product, name: "#{Faker::Name.name}#{search}#{Faker::Name.name}") }
+    it { expect(subject.search_by(query: search.upcase)).to include(model) }
   end
 end
