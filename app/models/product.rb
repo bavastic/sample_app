78609f8b9dd5801a32627ae6138ebaa697ec8526
Mikhail Mandronov <mikhail.mandronov@riskmethods.net>
2019-05-17 17:22:50 +0200

    Remove alases and serialization methods from models, add search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,24 +1,35 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: products
+#
+#  id               :bigint           not null, primary key
+#  category_id      :bigint
+#  name             :string           not null
+#  price            :decimal(, )
+#  currency         :string           default("EUR")
+#  display_currency :string           default("EUR")
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Product < ApplicationRecord
   belongs_to :category, counter_cache: true
 
   validates :name, presence: true
 
-  alias_attribute :text, :name
-  alias_attribute :value, :id
+  before_save :default_values
 
-  alias_attribute :displayName, :name
-  alias_attribute :categoryId, :category_id
-  alias_attribute :categoryName, :category_name
-  alias_attribute :displayCurrency, :display_currency
+  scope :search_by_name, ->(name) { where("name ILIKE ?", "%#{name}%") }
 
-  delegate :name, to: :category, prefix: true
+  def self.search_by(query:)
+    search_by_name(query)
+  end
 
-  def as_json(options = {})
-    super(
-      options.keys.any? ? options : {
-        only: [:id, :name, :currency, :price],
-        methods: [:displayName, :categoryId, :categoryName, :displayCurrency],
-      }
-    )
+  private
+
+  def default_values
+    self.price ||= 0
   end
 end
