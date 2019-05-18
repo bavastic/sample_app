66d33a2fbbb1227d854548ee86c6ce5e5120ee91
Mikhail Mandronov <mikhail.mandronov@riskmethods.net>
2019-05-18 09:11:36 +0200

    Move the catch of errors to base class

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,65 +3,42 @@
 class CategoriesController < ApplicationController
   def count
     render json: { count: service.categories_count }
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot count categories: unknown error!' } }, status: :internal_server_error
   end
 
   def index
     paginated_categories = service.fetch!(search: params[:query], sort: { id: :asc }, pagination: pagination_attr)
+
     render json: paginated_categories.collection
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot list categories: unknown error!' } }, status: :internal_server_error
   end
 
   def show
     category = service.find!(category_id: params[:id])
 
     render json: category
-  rescue ActiveRecord::RecordNotFound
-    render json: { notification: { level: 'error', message: 'Cannot show category: record not found!' } }, status: :not_found
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot show category: unknown error!' } }, status: :internal_server_error
   end
 
   def destroy
     category = service.destroy!(category_id: params[:id])
 
     render json: category
-  rescue ActiveRecord::RecordNotFound
-    render json: { notification: { level: 'error', message: 'Cannot destroy category: record not found!' } }, status: :not_found
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot destroy category: unknown error!' } }, status: :internal_server_error
   end
 
   def update
     category = service.update!(category_id: params[:id], category_attr: category_attr)
 
     render json: category
-  rescue ActiveRecord::RecordInvalid
-    render json: { notification: { level: 'error', message: 'Cannot update category!' } }, status: :not_acceptable
-  rescue ActiveRecord::RecordNotFound
-    render json: { notification: { level: 'error', message: 'Cannot delete category: record not found!' } }, status: :not_found
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot delete category: unknown error!' } }, status: :internal_server_error
   end
 
   def create
     category = service.create!(category_attr: category_attr)
 
     render json: category
-  rescue ActiveRecord::RecordInvalid
-    render json: { notification: { level: 'error', message: 'Cannot create category!' } }, status: :not_acceptable
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot delete category: unknown error!' } }, status: :internal_server_error
   end
 
   def options
     paginated_categories = service.fetch!(sort: { name: :asc })
 
     render json: paginated_categories.collection, each_serializer: CategoryOptionSerializer
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot fetch categories: unknown error!' } }, status: :internal_server_error
   end
 
   private
@@ -71,10 +48,10 @@ class CategoriesController < ApplicationController
   end
 
   def category_params
-    @category_params ||= params.require(:category).permit(:name, :parentId)
+    params.require(:category).permit(:name, :parentId)
   end
 
   def service
-    @sevice ||= CategoryService.new
+    @service ||= CategoryService.new
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,73 +1,44 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
-  # GET /products/count
   def count
     render json: { count: service.products_count }
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot count products: unknown error!' } }, status: :internal_server_error
   end
 
-  # GET /products
   def index
     paginated_products = service.fetch!(search: params[:query], sort: { id: :asc }, pagination: pagination_attr)
+
     render json: paginated_products.collection
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot list products: unknown error!' } }, status: :internal_server_error
   end
 
-  # GET /products/:id
   def show
     product = service.find!(product_id: params[:id])
 
     render json: product
-  rescue ActiveRecord::RecordNotFound
-    render json: { notification: { level: 'error', message: 'Cannot show product: record not found!' } }, status: :not_found
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot show product: unknown error!' } }, status: :internal_server_error
   end
 
-  # DELETE /products/:id
   def destroy
     product = service.destroy!(product_id: params[:id])
 
     render json: product
-  rescue ActiveRecord::RecordNotFound
-    render json: { notification: { level: 'error', message: 'Cannot destroy product: record not found!' } }, status: :not_found
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot destroy product: unknown error!' } }, status: :internal_server_error
   end
 
-  # PUT /products/:id
   def update
     product = service.update!(product_id: params[:id], product_attr: product_attr)
 
     render json: product
-  rescue ActiveRecord::RecordInvalid
-    render json: { notification: { level: 'error', message: 'Cannot update product!' } }, status: :not_acceptable
-  rescue ActiveRecord::RecordNotFound
-    render json: { notification: { level: 'error', message: 'Cannot delete product: record not found!' } }, status: :not_found
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot delete product: unknown error!' } }, status: :internal_server_error
   end
 
-  # POST /products
   def create
     product = service.create!(product_attr: product_attr)
 
     render json: product
-  rescue ActiveRecord::RecordInvalid
-    render json: { notification: { level: 'error', message: 'Cannot create product!' } }, status: :not_acceptable
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot delete product: unknown error!' } }, status: :internal_server_error
   end
 
-  # GET /products/options
   def options
     paginated_products = service.fetch!(sort: { name: :asc })
+
     render json: paginated_products.collection, each_serializer: ProductOptionSerializer
-  rescue Exception
-    render json: { notification: { level: 'error', message: 'Cannot fetch products: unknown error!' } }, status: :internal_server_error
   end
 
   private
@@ -81,6 +52,6 @@ class ProductsController < ApplicationController
   end
 
   def service
-    @sevice ||= ProductService.new
+    @service ||= ProductService.new
   end
 end
