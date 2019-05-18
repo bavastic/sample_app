1a62190bbf4cb0d698592516a39cdef99cb390b5
Mikhail Mandronov <mikhail.mandronov@riskmethods.net>
2019-05-18 09:11:03 +0200

    Define the catch of errors for controllers actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from Exception do
+    render_error(
+      message: "#{error_target}: unknown error!",
+      status: :internal_server_error
+    )
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do
+    render_error(
+      message: "#{error_target}!",
+      status: :not_acceptable
+    )
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render_error(
+      message: "#{error_target}: record not found!",
+      status: :not_found
+    )
+  end
+
   private
 
   def hash_keys_to_snake_case(hash:)
@@ -11,6 +32,15 @@ class ApplicationController < ActionController::Base
 
   def pagination_attr
     return {} unless params[:page] && params[:per_page]
+
     { current_page: params[:page], page_size: params[:per_page] }
+  end
+
+  def error_target
+    I18n.t("errors.#{controller_name}.#{action_name}")
+  end
+
+  def render_error(message:, status:)
+    render json: { notification: { level: 'error', message: message } }, status: status
   end
 end
